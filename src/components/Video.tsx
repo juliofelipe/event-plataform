d131c9d7460726655cbd3f982f1208e449bb814a
Julio Felipe <julio@informach.com.br>
2022-06-29 13:58:11 -0300

    feat: page event finished

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,14 +2,60 @@ import { Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
+import { gql, useQuery } from "@apollo/client";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug($slug: String) {
+  lesson(where: {slug: $slug}) {
+    title
+    videoId
+    description
+    teacher {
+      bio
+      avatarURL
+      name
+    }
+  }
+}
+`
+
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string
+        teacher: {
+          bio: string;
+          avatarURL: string;
+          name: string;
+        }
+    }
+}
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+        variables: {
+            slug: props.lessonSlug,
+        }
+    })
+    
+    if(!data) {
+        return (
+            <div className="flex-1">
+                <p>Carregando...</p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId="LAnXoXpcJJc"/>
+                        <Youtube videoId={data.lesson.videoId}/>
                     </Player>
                 </div>
             </div> 
@@ -18,22 +64,22 @@ export function Video() {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Aula 01 - Abertura do Ignite Lab
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, aperiam sequi? Quas nisi, vero quasi deleniti commodi enim assumenda debitis corrupti, veniam libero esse provident rem laudantium iure labore voluptas!
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
                             <img 
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/juliofelipe.png" 
+                                src={data.lesson.teacher.avatarURL} 
                                 alt="" 
                             />
 
                             <div className="loading-relaxed">
-                                <strong className="font-bold text-2xl block">Julio Felipe</strong>
-                                <span className="text-gray-200 text-sm block">CEO @Informach</span>
+                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                             </div>
                         </div>
                     </div>
