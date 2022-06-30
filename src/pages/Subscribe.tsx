770e509daaca4fc48e837cb196feb9a3b51c61c8
Julio Felipe <julio@informach.com.br>
2022-06-30 07:07:47 -0300

    chore: add graphql codegen

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -17,17 +17,19 @@ export function Subscribe() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
-    const [createSubscriber, ] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+    const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
 
-    function handleSubscribe(event: FormEvent) {
+    async function handleSubscribe(event: FormEvent) {
         event.preventDefault();
 
-        createSubscriber({
+        await createSubscriber({
             variables: {
                 name,
                 email,
             }
         })
+
+        navigate('/event')
     }
 
     return (
@@ -64,7 +66,8 @@ export function Subscribe() {
 
                         <button 
                             type="submit"
-                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                            disabled={loading}
+                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                         >
                             Garantir minha vaga
                         </button>
